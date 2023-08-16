81a39a06e88fb43c4cd9cb2a254f14b9e8b01bde
JoshP279 <138436491+JoshP279@users.noreply.github.com>
2023-08-16 14:16:13 +0200

    Quiz Quest - 2023/08/12

--- app/src/main/java/wrrv/quizquest/MainActivity.java
+++ app/src/main/java/wrrv/quizquest/MainActivity.java
@@ -4,10 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private TextView txtCoinsNum;
