2b93ec1434a5a1db3acac78d9f830928e989c737
marisha2000 <marisharanchod1@gmail.com>
2023-09-23 11:46:26 +0200

    Admin assess submissions

---
+++ app/src/main/java/wrrv/quizquest/AssessSubs_screen.java
@@ -0,0 +1,14 @@
+package wrrv.quizquest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AssessSubs_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assess_subs_screen);
+    }
+}
