a77323afeccc810d378e92d619faf0c7c6616cac
marisha2000 <marisharanchod1@gmail.com>
2023-09-27 23:15:40 +0200

    Added shoe array

--- app/src/main/java/wrrv/quizquest/SpriteGenerator.java
+++ app/src/main/java/wrrv/quizquest/SpriteGenerator.java
@@ -3,8 +3,35 @@ package wrrv.quizquest;
 import android.media.Image;
 
 public class SpriteGenerator {
-    Image[] shirts;
-    Image[] pants;
-    Image[] shoes;
+    int[] shirts;
+    int[] pants;
+    int[] shoes = {
+            R.drawable.f0,
+            R.drawable.f1,
+            R.drawable.f2,
+            R.drawable.f3,
+            R.drawable.f4,
+            R.drawable.f5,
+            R.drawable.f6,
+            R.drawable.f7,
+            R.drawable.f8,
+            R.drawable.f9,
+            R.drawable.f10,
+            R.drawable.f11,
+            R.drawable.f12,
+            R.drawable.f13,
+            R.drawable.f14,
+            R.drawable.f15,
+            R.drawable.f16,
+            R.drawable.f17,
+            R.drawable.f18,
+            R.drawable.f19,
+            R.drawable.f20,
+            R.drawable.f21,
+            R.drawable.f22,
+            R.drawable.f23
+    };
+
+
 
 }
