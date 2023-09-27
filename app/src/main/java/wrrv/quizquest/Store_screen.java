15150f761128a1c511289e264306053620ec84ce
Marisha <marisharanchod1@gmail.com>
2023-09-27 10:14:14 +0200

    Store screen

--- app/src/main/java/wrrv/quizquest/Store_screen.java
+++ app/src/main/java/wrrv/quizquest/Store_screen.java
@@ -1,14 +1,65 @@
 package wrrv.quizquest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class Store_screen extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager2 viewpager2;
+    StoreAdapter adapter;
+
+    ImageButton leave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_screen);
+
+        leave =findViewById(R.id.btnLeaveStore);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewpager2 = findViewById(R.id.viewPager2);
+
+        adapter = new StoreAdapter(this);
+        viewpager2.setAdapter(adapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewpager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewpager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
+
+    }
+
+    public void leaveClick(View view){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
