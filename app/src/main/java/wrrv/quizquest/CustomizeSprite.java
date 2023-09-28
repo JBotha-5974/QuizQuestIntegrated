c32bb2cb131c6336227717a81c2c8dcae4f4443d
JBotha-5974 <bothajoshua@gmail.com>
2023-09-28 17:51:58 +0200

    Other changes: Updated so user can login instead of make a profile. Added layout back as well as class that is removed.

--- app/src/main/java/wrrv/quizquest/CustomizeSprite.java
+++ app/src/main/java/wrrv/quizquest/CustomizeSprite.java
@@ -3,6 +3,7 @@ package wrrv.quizquest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class CustomizeSprite extends AppCompatActivity {
 
@@ -10,5 +11,35 @@ public class CustomizeSprite extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customize_sprite);
+    }
+
+    public void btnFeetBackClick(View view) {
+    }
+
+    public void btnHairBackClick(View view) {
+    }
+
+    public void btnShirtBackClick(View view) {
+    }
+
+    public void btnPantsBackClick(View view) {
+    }
+
+    public void btnHairForwardClick(View view) {
+    }
+
+    public void btnCustomizeCancelClick(View view) {
+    }
+
+    public void btnCustomizeConfirmClick(View view) {
+    }
+
+    public void btnShirtForwardClick(View view) {
+    }
+
+    public void btnPantsForwardClick(View view) {
+    }
+
+    public void btnFeetForwardClick(View view) {
     }
 }
