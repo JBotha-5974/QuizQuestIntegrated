2ab73c2a1ec93564255233d388bfbd1ca959ad54
marisha2000 <marisharanchod1@gmail.com>
2023-09-29 07:54:12 +0200

    player performance

--- app/src/main/java/wrrv/quizquest/PlayerPerformance.java
+++ app/src/main/java/wrrv/quizquest/PlayerPerformance.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class PlayerPerformance extends AppCompatActivity {
@@ -13,10 +14,14 @@ public class PlayerPerformance extends AppCompatActivity {
     private TextView quizzesDoneTxt;
     private TextView pointsTxt;
     private TextView coinsTxt;
+    private ImageView sprite;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_performance);
+        GenerateSprite generator;
         Intent intent = getIntent();
         if (intent != null){
             Player player = (Player) intent.getSerializableExtra("player");
@@ -29,6 +34,9 @@ public class PlayerPerformance extends AppCompatActivity {
                 quizzesDoneTxt = findViewById(R.id.quizzesDoneTxt);
                 pointsTxt = findViewById(R.id.pointsTxt);
                 coinsTxt = findViewById(R.id.coinsTxt);
+                sprite = findViewById(R.id.imageView);
+                generator = new GenerateSprite(this, player.getPlayerSprite());
+                sprite.setBackground(generator.getImage());
                 questionsCorrectTxt.setText(getString(R.string.questions_correct,score));
                 quizzesDoneTxt.setText(getString(R.string.quiz_num_for_today,quizzes));
                 pointsTxt.setText(getString(R.string.points,score));
