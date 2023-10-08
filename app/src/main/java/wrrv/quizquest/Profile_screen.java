b3c1e06c6f54a2a554f349af8a4c17b435960c7c
marisha2000 <marisharanchod1@gmail.com>
2023-10-08 12:46:58 +0200

    Added username on profile screen

--- app/src/main/java/wrrv/quizquest/Profile_screen.java
+++ app/src/main/java/wrrv/quizquest/Profile_screen.java
@@ -3,14 +3,21 @@ package wrrv.quizquest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Profile_screen extends AppCompatActivity {
 
     Button submissions;
-    Button leave;
+    ImageButton leave;
+    TextView username;
+
+    Player player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +25,27 @@ public class Profile_screen extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         //temporary just to get to submissions
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String savedUsername = sharedPreferences.getString("username", "");
+        String savedPassword = sharedPreferences.getString("password", "");
+        try {
+            player = Database.getPlayer(savedUsername,savedPassword);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         submissions = findViewById(R.id.btnSubmissions);
+        leave = findViewById(R.id.btnLeaveProfile);
+        username = findViewById(R.id.lblUsername);
+
+        username.setText("Hello " + player.getUserName() + "!");
 
         submissions.setOnClickListener(view ->{
             openSubmissions(view);
+        });
+
+        leave.setOnClickListener(view ->{
+            leaveClick(view);
         });
     }
 
