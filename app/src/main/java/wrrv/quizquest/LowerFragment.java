63a35f7faf19639a5d6d68ecba2c105eedacb0ec
marisha2000 <marisharanchod1@gmail.com>
2023-10-21 17:45:55 +0200

    Other fragments temp lists

--- app/src/main/java/wrrv/quizquest/LowerFragment.java
+++ app/src/main/java/wrrv/quizquest/LowerFragment.java
@@ -1,14 +1,24 @@
 package wrrv.quizquest;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-public class LowerFragment extends Fragment {
+import java.util.ArrayList;
+
+public class LowerFragment extends Fragment implements ItemAdapter.OnItemClickListener{
+
+    ArrayList<Item> items;
+    RecyclerView rvLower;
+    ItemAdapter adapter;
 
     public LowerFragment() {
         // Required empty public constructor
@@ -17,7 +27,62 @@ public class LowerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lower, container, false);
+        View view = inflater.inflate(R.layout.fragment_lower, container, false);
+
+        rvLower = view.findViewById(R.id.rvLower);
+        rvLower.setLayoutManager(
+                new GridLayoutManager(getContext(),2));
+
+        getItems();
+
+        adapter = new ItemAdapter(items, this);
+        rvLower.setAdapter(adapter);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onItemClick(Item item) {
+        // Handle item click here
+        // Start the SecondActivity when an item is clicked
+
+        String activityName = getActivity().getClass().getSimpleName();
+
+        if(activityName.equals("Store_screen")){
+            //if player
+
+            Intent intent = new Intent(getContext(),View_Item.class);
+            intent.putExtra("Item", item);
+            startActivity(intent);
+        }
+        else if(activityName.equals("Admin_Store")){
+            //if admin
+            Intent intent = new Intent(getContext(),Update_Item.class);
+            intent.putExtra("Item", item);
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(getContext(), "Invalid activity: " + activityName, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void getItems(){
+        items = new ArrayList<>();
+
+        try{
+            items = Database.getItems("shoes");
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/wrrv/quizquest/UpperFragment.java
+++ app/src/main/java/wrrv/quizquest/UpperFragment.java
@@ -1,15 +1,25 @@
 package wrrv.quizquest;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
-public class UpperFragment extends Fragment {
+public class UpperFragment extends Fragment implements ItemAdapter.OnItemClickListener{
+
+    ArrayList<Item> items;
+    RecyclerView rvUpper;
+    ItemAdapter adapter;
 
     public UpperFragment() {
         // Required empty public constructor
@@ -18,7 +28,62 @@ public class UpperFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upper, container, false);
+        View view = inflater.inflate(R.layout.fragment_upper, container, false);
+
+        rvUpper = view.findViewById(R.id.rvUpper);
+        rvUpper.setLayoutManager(
+                new GridLayoutManager(getContext(),2));
+
+        getItems();
+
+        adapter = new ItemAdapter(items, this);
+        rvUpper.setAdapter(adapter);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onItemClick(Item item) {
+        // Handle item click here
+        // Start the SecondActivity when an item is clicked
+
+        String activityName = getActivity().getClass().getSimpleName();
+
+        if(activityName.equals("Store_screen")){
+            //if player
+
+            Intent intent = new Intent(getContext(),View_Item.class);
+            intent.putExtra("Item", item);
+            startActivity(intent);
+        }
+        else if(activityName.equals("Admin_Store")){
+            //if admin
+            Intent intent = new Intent(getContext(),Update_Item.class);
+            intent.putExtra("Item", item);
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(getContext(), "Invalid activity: " + activityName, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void getItems(){
+        items = new ArrayList<>();
+
+        try{
+            items = Database.getItems("shoes");
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
