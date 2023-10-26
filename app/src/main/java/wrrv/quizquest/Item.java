abcde03dfefdf7b33ec657ede0484fe8f6b6ad80
marisha2000 <marisharanchod1@gmail.com>
2023-10-26 14:39:07 +0200

    Add unversioned files

--- app/src/main/java/wrrv/quizquest/Item.java
+++ app/src/main/java/wrrv/quizquest/Item.java
@@ -1,8 +1,12 @@
 package wrrv.quizquest;
 
+import android.graphics.BitmapFactory;
 import android.media.Image;
-
+import android.graphics.Bitmap;
+import android.os.Bundle;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Item implements Serializable {
     private int itemID;
@@ -13,12 +17,65 @@ public class Item implements Serializable {
     private String array;
     private int position;
 
-    public Item(int itemID, String itemName, int itemPrice, String array, int position) {
+    //making it so the player just buys 1 item and can have all the colours
+    String colors;
+    Set<String> options;
+    String curColor;
+
+    public Item(int itemID, String itemName, int itemPrice, String array, int position, String colors, String curColor) {
         this.itemID = itemID;
         this.itemName = itemName;
         this.itemPrice = itemPrice;
         this.array = array;
         this.position = position;
+        this.colors = colors;
+        this.curColor = curColor;
+
+        setColors();
+    }
+
+
+
+    private void setColors(){
+        String[] c = colors.split(",");
+
+        options = new HashSet<>();
+
+        for (String option : c) {
+            options.add(option);
+        }
+    }
+
+    public int getItemImage() {
+        SpriteGenerator generator = new SpriteGenerator();
+
+        return generator.getImage(array, position);
+
+    }
+
+    public Bitmap getItemBitmap(){
+
+    }
+
+    private Image[] splitImage(int imageResourceId){
+
+        int rows = 21;
+        int columns = 13;
+
+        Bitmap image = BitmapFactory.decodeResource(getResources(), imageResourceId);
+
+        int subimage_Width = image.getWidth() / columns;
+        int subimage_Height = image.getHeight() / rows;
+
+    }
+
+    public void setUpdate(int id, String desc, int price){
+        setItemName(desc);
+        setItemPrice(price);
+
+        // update in db
+        Database.updateItem(id,desc,price);
+
     }
 
     public int getItemID() {
@@ -43,21 +100,5 @@ public class Item implements Serializable {
 
     public void setItemPrice(int itemPrice) {
         this.itemPrice = itemPrice;
-    }
-
-    public int getItemImage() {
-        SpriteGenerator generator = new SpriteGenerator();
-
-        return generator.getImage(array, position);
-
-    }
-
-    public void setUpdate(int id, String desc, int price){
-        setItemName(desc);
-        setItemPrice(price);
-
-        // update in db
-        Database.updateItem(id,desc,price);
-
     }
 }
--- app/src/main/java/wrrv/quizquest/SpriteGenerator.java
+++ app/src/main/java/wrrv/quizquest/SpriteGenerator.java
@@ -1,48 +1,48 @@
 package wrrv.quizquest;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Set;
+
 public class SpriteGenerator {
-    int[] shirts;
-    int[] pants;
-    int[] shoes = {
-            R.drawable.f0,
-            R.drawable.f1,
-            R.drawable.f2,
-            R.drawable.f3,
-            R.drawable.f4,
-            R.drawable.f5,
-            R.drawable.f6,
-            R.drawable.f7,
-            R.drawable.f8,
-            R.drawable.f9,
-            R.drawable.f10,
-            R.drawable.f11,
-            R.drawable.f12,
-            R.drawable.f13,
-            R.drawable.f14,
-            R.drawable.f15,
-            R.drawable.f16,
-            R.drawable.f17,
-            R.drawable.f18,
-            R.drawable.f19,
-            R.drawable.f20,
-            R.drawable.f21,
-            R.drawable.f22,
-            R.drawable.f23
-    };
+
+    //private Map<String, Set<String>> item;
 
     public int getImage(String array, int position){
-        switch(array){
-            case "shoes":
-                return shoes[position];
-            default:
-                return shoes[position];
+//        switch(array){
+//            case "shoes":
+//                return shoes[position];
+//            default:
+//                return shoes[position];
+//
+//
+//        }
+        return 1;
+    }
 
+    public SpriteGenerator(){
 
-        }
     }
 
 
+    public static Bitmap getItemBitmap(Context context, int resourceId) {
+        try {
+            // Open an input stream to the asset
+            InputStream inputStream = getAssets().open("image.jpg");
+
+            // Decode the InputStream into a Bitmap
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+            // Now 'bitmap' contains the loaded image
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
