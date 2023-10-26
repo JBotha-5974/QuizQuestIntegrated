04a723e49b51f9ee444cc98dd4c440ccf49dc2d0
marisha2000 <marisharanchod1@gmail.com>
2023-10-26 20:45:11 +0200

    item images

---
+++ app/src/main/java/wrrv/quizquest/tester.java
@@ -0,0 +1,14 @@
+package wrrv.quizquest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tester extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tester);
+    }
+}
