b92c9fb313b01f87f1b5154ac1f748d6e7638bbd
marisha2000 <marisharanchod1@gmail.com>
2023-10-26 21:12:12 +0200

    item layer comparator

---
+++ app/src/main/java/wrrv/quizquest/ItemLayerComparator.java
@@ -0,0 +1,11 @@
+package wrrv.quizquest;
+
+import java.util.Comparator;
+
+public class ItemLayerComparator implements Comparator<Item> {
+    @Override
+    public int compare(Item item1, Item item2) {
+
+        return Integer.compare(item1.getLayer(), item2.getLayer());
+    }
+}
