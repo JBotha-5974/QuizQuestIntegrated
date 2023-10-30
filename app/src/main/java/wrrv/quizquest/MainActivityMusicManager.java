2dbd405cc67e05ee7fa6e674a39130836994f42f
JBotha-5974 <bothajoshua@gmail.com>
2023-10-30 12:58:52 +0200

    Adjust Music Service

--- app/src/main/java/wrrv/quizquest/MainActivityMusicManager.java
+++ app/src/main/java/wrrv/quizquest/MainActivityMusicManager.java
@@ -3,25 +3,31 @@ package wrrv.quizquest;
 import android.app.*;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class MainActivityMusicManager extends Service {
+    private final IBinder binder = new LocalBinder();
     private static final String TAG = null;
     MediaPlayer player;
     public IBinder onBind(Intent arg0) {
 
         return null;
     }
+    public class LocalBinder extends Binder {
+        MainActivityMusicManager getService() {
+            // Return this instance of LocalService so clients can call public methods.
+            return MainActivityMusicManager.this;
+        }
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
-
-
         player = MediaPlayer.create(this, R.raw.play);
         player.setLooping(true); // Set looping
         player.setVolume(100,100);
         player.start();
-
     }
     public int onStartCommand(Intent intent, int flags, int startId) {
         player.start();
@@ -31,13 +37,13 @@ public class MainActivityMusicManager extends Service {
     public void onStart(Intent intent, int startId) {
         // TODO
 
-
-
+    }
+    public void setVolume(int iVal)
+    {
+        player.setVolume(iVal,iVal);
     }
     public IBinder onUnBind(Intent arg0) {
-        // TODO Auto-generated method stub
-
-        return null;
+        return binder;
     }
 
     public void onStop() {
--- app/src/main/java/wrrv/quizquest/Settings.java
+++ app/src/main/java/wrrv/quizquest/Settings.java
@@ -2,12 +2,17 @@ package wrrv.quizquest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
+
+import java.security.Provider;
 
 public class Settings extends AppCompatActivity {
 
@@ -22,13 +27,41 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
+        Service music = new MainActivityMusicManager();
+        music.onCreate();
         musicLevel = findViewById(R.id.musicLevel);
         logOut = findViewById(R.id.btnPlayerLogOut);
         credits = findViewById(R.id.btnCredits);
         leave = findViewById(R.id.btnLeaveSettings);
+        @SuppressLint("WrongConstant") AudioManager manager = (AudioManager) getSystemService("MainActivityMusicManager");
 
+        if (manager == null) {
+            // Handle the case where the service is not available
+        } else {
+            int maxVolume = manager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+            musicLevel.setMax(maxVolume);
+            int currVolume = manager.getStreamVolume(AudioManager.STREAM_MUSIC);
+            musicLevel.setProgress(currVolume);
 
+            // Set up your SeekBar listener here as you did before
+            musicLevel.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                    manager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+                    ((MainActivityMusicManager) music).setVolume(AudioManager.STREAM_MUSIC);
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                    // Do Nothing
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    // Do Nothing
+                }
+            });
+        }
     }
     public void openCredits(View view){
         Intent intent = new Intent(this,Credits.class);
